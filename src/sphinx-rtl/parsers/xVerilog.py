b105e7d271b75cc0d45ce06c5291403908553ad5
l.heywang <leonard.heywang@proton.me>
2026-09-21 21:33:35 +0200

    Done the ANSI port parsing. Need to finish the parser

--- src/sphinx-rtl/parsers/xVerilog.py
+++ src/sphinx-rtl/parsers/xVerilog.py
@@ -8,6 +8,7 @@
 # Imports
 import pyslang.ast as ast
 import pyslang.syntax as syntax
+from pyslang import SourceManager
 import logging
 from pathlib import Path
 
@@ -16,13 +17,6 @@ from .xParser import xParser
 
 # Logger config
 logger = logging.getLogger(__name__)
-
-
-def to_text(cst_node) -> str:
-    if cst_node is None:
-        return ""
-    # Instance neuve à chaque appel = buffer vierge
-    return syntax.SyntaxPrinter().print(cst_node).str().strip()
 
 
 class xVerilogParser(xParser):
@@ -38,6 +32,12 @@ class xVerilogParser(xParser):
 
         # No tools are needed, they're handled by the wheel !
         super().__init__()
+
+        # Init the elements to induce the memory effect between the calls:
+        self.port = Port("unknown", "unknown", "unknown", ["none"])
+
+        # Append the source manager :
+        self.sm: SourceManager = SourceManager()
 
     # ----------------------------------------------------------------------------
     # COMMENTS PARSERS
@@ -113,6 +113,122 @@ class xVerilogParser(xParser):
         return comments
 
     # ----------------------------------------------------------------------------
+    # SYMBOLS BUILDERS
+    # ----------------------------------------------------------------------------
+    def build_port(self, node: ast.PortSymbol) -> Port:
+        """
+        Build a port object from the passed source !
+        """
+
+        # Get default values
+        if node.name != "":
+            self.port.name = node.name
+
+        # Get the direction
+        match node.direction:
+            case ast.ArgumentDirection.In:
+                self.port.direction = "input"
+            case ast.ArgumentDirection.Out:
+                self.port.direction = "output"
+            case ast.ArgumentDirection.InOut:
+                self.port.direction = "inout"
+
+        # Two cases : If the port is declared as ANSI, or not.
+        if node.isAnsiPort:
+
+            # If Ansi port, the parameters are in the PortDeclarationSyntax:
+            node_syntax: syntax.ImplicitAnsiPortSyntax = node.syntax.parent
+            node_header: syntax.PortHeaderSyntax = node_syntax.header
+
+            # We need to process the dimension of the "global" definition:
+            raw_syntax = str(node_header.dataType).strip().split(" ", 1)
+
+            # Update the type
+            if raw_syntax[0] != "":
+                self.port.hdl_type = raw_syntax[0].strip()
+
+            # Extract the dimensions
+            if len(raw_syntax) > 1:
+
+                # Extract each pairs
+                size_pairs = [
+                    x.strip() for x in raw_syntax[1].replace("[", "").split("]")
+                ]
+
+                # Clear the list
+                self.port.hdl_size = []
+
+                # For each pairs, append one to the port
+                for size_pair in size_pairs:
+                    bounds = [x.strip() for x in size_pair.split(":")]
+
+                    # If there's at least two bounds
+                    if len(bounds) >= 2:
+                        self.port.hdl_size.append(bounds[0])
+                        self.port.hdl_size.append(bounds[1])
+
+            else:
+                self.port.hdl_size = ["0", "0"]
+
+            # Finally, processing the last elements (a size that may be specific to the declaration)
+            node_declarator: syntax.DeclaratorSyntax = node_syntax.declarator
+            for dimension in node_declarator.dimensions:
+
+                raw_dimension = str(dimension)
+                size_pairs = [
+                    x.strip()
+                    for x in raw_dimension.replace("[", "").split("]")
+                    if len(x) > 1
+                ]
+
+                # Attempt to split the pairs, if fail that's a Scalar
+                for size_pair in size_pairs:
+
+                    bounds = [x.strip() for x in size_pair.split(":")]
+
+                    # scalar
+                    if len(bounds) == 1:
+                        self.port.hdl_size.append(f"{int(bounds[0]) - 1}")
+                        self.port.hdl_size.append("0")
+
+                    elif len(bounds) == 2:
+                        self.port.hdl_size.append(bounds[0])
+                        self.port.hdl_size.append(bounds[1])
+
+            # Add the line
+            source = self.sm.getLineNumber(node_declarator.sourceRange.start)
+            if source > 0:
+                self.port.line = source
+            else:
+                self.port.line = -1
+
+        else:
+
+            pass
+
+        # Build the port
+        print(self.port)
+        return self.port
+
+        """
+        header_node = m.syntax
+                                    print(type(m))
+                                    print(
+                                        f"En-tête (m.syntax)           : {to_text(header_node), m.direction}"
+                                    )
+                                    print(m.isAnsiPort)
+        
+                                    # 2. La déclaration réelle avec le type dans le corps
+                                    internal = getattr(m, "internalSymbol", None)
+                                    if internal and internal.syntax:
+                                        # internal.syntax est le signal, son parent est la ligne complète (ex: input logic [XLEN-1:0] addr_in;)
+                                        body_decl_node = internal.syntax.parent
+                                        print(
+                                            f"Corps (internalSymbol.parent) : {to_text(body_decl_node)}"
+                                        )
+        """
+
+    # ----------------------------------------------------------------------------
     # GLOBAL PARSER
     # ----------------------------------------------------------------------------
 
@@ -149,6 +265,7 @@ class xVerilogParser(xParser):
 
         # Iterate over the different nodes :
         root = compilation.getRoot()
+        self.sm = compilation.sourceManager
         for instance in root.topInstances:
 
             # List to track if the port name is already known, or not ?!?
@@ -169,19 +286,11 @@ class xVerilogParser(xParser):
                             print(f"Import : {type(m).__name__}")
 
                         case ast.SymbolKind.Port:
-                            header_node = m.syntax
-                            print(
-                                f"En-tête (m.syntax)           : {to_text(header_node)}"
-                            )
+                            # Backup the port name
+                            ports_names.append(m.name)
 
-                            # 2. La déclaration réelle avec le type dans le corps
-                            internal = getattr(m, "internalSymbol", None)
-                            if internal and internal.syntax:
-                                # internal.syntax est le signal, son parent est la ligne complète (ex: input logic [XLEN-1:0] addr_in;)
-                                body_decl_node = internal.syntax.parent
-                                print(
-                                    f"Corps (internalSymbol.parent) : {to_text(body_decl_node)}"
-                                )
+                            # Build the port
+                            ports.append(self.build_port(m))
 
                         case ast.SymbolKind.Net | ast.SymbolKind.Variable:
                             print(f"Signal : {type(m).__name__}")
